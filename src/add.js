ff5cab868851fde24bcb4291cbd8b55c51c82d6a
Renat <suleymanov_renat@mail.ru>
2020-03-14 18:00:11 +0300

    правки

--- src/add.js
+++ src/add.js
@@ -1,27 +1,25 @@
 export default findBy;
 
 function findBy(key, value) {
-  return person => person[key] === value;
+  return (person) => person[key] === value;
 }
 
-let finder = findBy("name", "маг");
+const finder = findBy('name', 'маг');
 
 const results = [
   {
-    name: "маг",
-    type: "character",
-    description: "Персонаж, обладающий магическими способностями"
+    name: 'маг',
+    type: 'character',
+    description: 'Персонаж, обладающий магическими способностями',
   },
   {
-    name: "заклинание",
-    type: "attack",
-    description: "Атака магическим заклинанием"
+    name: 'заклинание',
+    type: 'attack',
+    description: 'Атака магическим заклинанием',
   },
   {
-    name: "урон",
-    type: "help",
-    description: "Страница описания элемента интерфейса"
-  }
+    name: 'урон',
+    type: 'help',
+    description: 'Страница описания элемента интерфейса',
+  },
 ].filter(finder);
-
-console.log(results);
--- test/test.js
+++ test/test.js
@@ -1,36 +1,33 @@
-import findBy from "../src/add";
+import findBy from '../src/add';
 
-test ("test person", () => {
-    let finder = findBy("name", "маг");
+test('test person', () => {
+  const finder = findBy('name', 'маг');
 
-    const received = [
-      {
-        name: "маг",
-        type: "character",
-        description: "Персонаж, обладающий магическими способностями"
-      },
-      {
-        name: "заклинание",
-        type: "attack",
-        description: "Атака магическим заклинанием"
-      },
-      {
-        name: "урон",
-        type: "help",
-        description: "Страница описания элемента интерфейса"
-      }
-    ].filter(finder);
+  const received = [
+    {
+      name: 'маг',
+      type: 'character',
+      description: 'Персонаж, обладающий магическими способностями',
+    },
+    {
+      name: 'заклинание',
+      type: 'attack',
+      description: 'Атака магическим заклинанием',
+    },
+    {
+      name: 'урон',
+      type: 'help',
+      description: 'Страница описания элемента интерфейса',
+    },
+  ].filter(finder);
 
-    const expected = [
-      {
-        name: "маг",
-        type: "character",
-        description: "Персонаж, обладающий магическими способностями"
-      }
-    ];
+  const expected = [
+    {
+      name: 'маг',
+      type: 'character',
+      description: 'Персонаж, обладающий магическими способностями',
+    },
+  ];
 
-    expect(received).toEqual(expected);
-})
-
-
-
+  expect(received).toEqual(expected);
+});
